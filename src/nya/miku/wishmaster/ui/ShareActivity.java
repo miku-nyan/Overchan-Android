11a9d7cb21b14efc8d17d68882c98c85aa6bbbc8
Miku Nyan <miku-nyan@yandex.ru>
2015-07-06 21:59:23 +0300

    sharing

--- src/nya/miku/wishmaster/ui/ShareActivity.java
+++ src/nya/miku/wishmaster/ui/ShareActivity.java
@@ -20,17 +20,37 @@ package nya.miku.wishmaster.ui;
 
 import java.io.File;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import nya.miku.wishmaster.R;
+import nya.miku.wishmaster.api.models.BoardModel;
+import nya.miku.wishmaster.api.models.SendPostModel;
+import nya.miku.wishmaster.api.models.UrlPageModel;
+import nya.miku.wishmaster.cache.SerializablePage;
+import nya.miku.wishmaster.common.MainApplication;
 import nya.miku.wishmaster.lib.UriFileUtils;
-import android.app.Activity;
+import nya.miku.wishmaster.ui.posting.PostFormActivity;
+import nya.miku.wishmaster.ui.posting.PostingService;
+import nya.miku.wishmaster.ui.tabs.TabModel;
+import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class ShareActivity extends Activity {
+public class ShareActivity extends ListActivity {
+    private File selectedFile;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(MainApplication.getInstance().settings.getTheme());
         handleIntent(getIntent());
     }
     
@@ -41,15 +61,95 @@ public class ShareActivity extends Activity {
     }
     
     private void handleIntent(Intent intent) {
+        ArrayAdapter<Pair<TabModel, SerializablePage>> adapter = new ArrayAdapter<Pair<TabModel, SerializablePage>>(this, 0) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = convertView == null ? getLayoutInflater().inflate(android.R.layout.simple_list_item_1, parent, false) : convertView;
+                TextView tv = (TextView) view.findViewById(android.R.id.text1);
+                tv.setEllipsize(TextUtils.TruncateAt.END);
+                tv.setSingleLine();
+                tv.setText(getItem(position).getLeft().title);
+                return view;
+            }
+        };
+        for (TabModel tab : MainApplication.getInstance().tabsState.tabsArray) {
+            if (tab.type == TabModel.TYPE_NORMAL && tab.pageModel.type != UrlPageModel.TYPE_SEARCHPAGE) {
+                SerializablePage page = MainApplication.getInstance().pagesCache.getSerializablePage(tab.hash);
+                if (page != null) {
+                    adapter.add(Pair.of(tab, page));
+                }
+            }
+        }
+        if (adapter.getCount() == 0) {
+            Toast.makeText(this, "No Tabs", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        if (PostingService.isNowPosting()) {
+            Toast.makeText(this, R.string.posting_now_posting, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        selectedFile = null;
         if (intent != null) {
             Uri uri = intent.getParcelableExtra(Intent.EXTRA_STREAM);
             if (uri != null) {
                 File file = UriFileUtils.getFile(this, uri);
                 if (file != null) {
-                    Toast.makeText(this, file.getPath(), Toast.LENGTH_LONG).show();
+                    selectedFile = file;
                 }
             }
         }
+        if (selectedFile == null) {
+            Toast.makeText(this, R.string.postform_cannot_attach, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        setListAdapter(adapter);
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        @SuppressWarnings("unchecked")
+        Pair<TabModel, SerializablePage> item = (Pair<TabModel, SerializablePage>) getListAdapter().getItem(position);
+        SendPostModel draft = MainApplication.getInstance().draftsCache.get(item.getLeft().hash);
+        if (draft == null) {
+            draft = new SendPostModel();
+            draft.chanName = item.getLeft().pageModel.chanName;
+            draft.boardName = item.getLeft().pageModel.boardName;
+            draft.threadNumber = item.getLeft().pageModel.type == UrlPageModel.TYPE_THREADPAGE ? item.getLeft().pageModel.threadNumber : null;
+            draft.comment = "";
+            BoardModel boardModel = item.getRight().boardModel;
+            if (boardModel.allowNames) draft.name = MainApplication.getInstance().settings.getDefaultName();
+            if (boardModel.allowEmails) draft.email = MainApplication.getInstance().settings.getDefaultEmail();
+            if (boardModel.allowDeletePosts || boardModel.allowDeleteFiles) draft.password =
+                    MainApplication.getInstance().getChanModule(item.getLeft().pageModel.chanName).getDefaultPassword();
+            if (boardModel.allowRandomHash) draft.randomHash = MainApplication.getInstance().settings.isRandomHash();
+        }
+        
+        BoardModel boardModel = item.getRight().boardModel;
+        int attachmentsCount = draft.attachments == null ? 0 : draft.attachments.length;
+        ++attachmentsCount;
+        if (attachmentsCount > boardModel.attachmentsMaxCount) {
+            Toast.makeText(this, R.string.postform_max_attachments, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        File[] attachments = new File[attachmentsCount];
+        for (int i=0; i<(attachmentsCount-1); ++i) attachments[i] = draft.attachments[i];
+        attachments[attachmentsCount-1] = selectedFile;
+        draft.attachments = attachments;
+        
+        if (PostingService.isNowPosting()) {
+            Toast.makeText(this, R.string.posting_now_posting, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        Intent addPostIntent = new Intent(this.getApplicationContext(), PostFormActivity.class);
+        addPostIntent.putExtra(PostingService.EXTRA_PAGE_HASH, item.getLeft().hash);
+        addPostIntent.putExtra(PostingService.EXTRA_BOARD_MODEL, item.getRight().boardModel);
+        addPostIntent.putExtra(PostingService.EXTRA_SEND_POST_MODEL, draft);
         finish();
+        startActivity(addPostIntent);
     }
 }
