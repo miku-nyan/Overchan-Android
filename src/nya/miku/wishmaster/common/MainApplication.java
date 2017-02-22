2388957bd46b0b7d451a9b841ba8d15c4643c195
Eilhart <whattahellisgoinon@gmail.com>
2017-02-22 19:20:00 +0600

    remove 0chan.cc module

--- src/nya/miku/wishmaster/common/MainApplication.java
+++ src/nya/miku/wishmaster/common/MainApplication.java
@@ -97,7 +97,6 @@ public class MainApplication extends Application {
             "nya.miku.wishmaster.chans.ponyach.PonyachModule",
             "nya.miku.wishmaster.chans.uchan.UchanModule",
             "nya.miku.wishmaster.chans.sich.SichModule",
-            "nya.miku.wishmaster.chans.nullchan.NullchanccModule",
             "nya.miku.wishmaster.chans.nullchan.NullchaneuModule",
             "nya.miku.wishmaster.chans.nullchan.Null_chanModule",
             "nya.miku.wishmaster.chans.dvachnet.DvachnetModule",
