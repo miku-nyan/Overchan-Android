39084abc99b78db2be179c5cfd4144a49086a109
AliceCA <aliceca@users.noreply.github.com>
2017-04-17 21:37:21 +0700

    Add new theme

--- src/nya/miku/wishmaster/ui/settings/ApplicationSettings.java
+++ src/nya/miku/wishmaster/ui/settings/ApplicationSettings.java
@@ -179,6 +179,7 @@ public class ApplicationSettings {
         if (theme.equals(resources.getString(R.string.pref_theme_value_gurochan))) return R.style.Theme_Gurochan;
         if (theme.equals(resources.getString(R.string.pref_theme_value_tomorrow))) return R.style.Theme_Tomorrow;
         if (theme.equals(resources.getString(R.string.pref_theme_value_mikuba))) return R.style.Theme_Mikuba;
+        if (theme.equals(resources.getString(R.string.pref_theme_value_dark_mint))) return R.style.Theme_Dark_Mint;
         return R.style.Theme_Futaba;
     }
     
